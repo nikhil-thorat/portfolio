927389fd479a0e21f2ce4ba3a255e1aff2006b9c
nikhil-thorat <nikhilthoratofficial@gmail.com>
2025-11-10 00:48:42 +0530

    fixed email

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,7 +4,7 @@ function Contact() {
     return <section className="group space-y-8">
         <Title>Connect</Title>
         <div>
-            <p>Reach me at <a href="mailto:nikhilthoratofficial@gmail.com" target="_blank" className="hover:underline decoration-2 decoration-blue-500 hover:text-neutral-700">work.niktilthorat@gmail.com</a> or
+            <p>Reach me at <a href="mailto:work.nikhilthorat@gmail.com" target="_blank" className="hover:underline decoration-2 decoration-blue-500 hover:text-neutral-700">work.nikhilthorat@gmail.com</a> or
                 <a href="https://x.com/iamnikhilthorat" target="_blank" className="hover:underline decoration-2 decoration-blue-500 hover:text-neutral-700"> @iamnikhilthorat</a>
             </p>
         </div>
