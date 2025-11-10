7a4d52e636cfb9ad284a97ea36344297965b151c
nikhil-thorat <nikhilthoratofficial@gmail.com>
2025-11-10 11:08:01 +0530

    improved project's look

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,16 +6,16 @@ function Projects() {
         <Title>Projects</Title>
         <div className="flex flex-col gap-12">
             {projects.map((project) => {
-                return <div key={project.github} className="flex flex-col gap-2">
-                    <a href={project.github} target="_blank" className="w-fit hover:underline decoration-2 underline-offset-2 decoration-blue-500">
+                return <div key={project.github} className="flex flex-col gap-2 group/project">
+                    <a href={project.github} target="_blank" className="w-fit group-hover/project:underline decoration-2 underline-offset-2 decoration-blue-500">
                         <h1 className="w-fit text-neutral-700">{project.title}</h1>
                     </a>
                     <p className="leading-7">
                         {project.description}
                     </p>
-                    <div className="space-x-2">
+                    <div className="space-x-4">
                         {project.tags.map((tag) => {
-                            return <span className="text-sm" key={tag}>{tag}</span>
+                            return <span className="text-sm italic bg-neutral-300/50 p-1.5 rounded-lg" key={tag}>{tag}</span>
                         })}
                     </div>
                 </div>
